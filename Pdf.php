d5810a125a5c21b25a00e4b52f0cc71a1ba0bded
Luis Gamboa Pacheco <luisgamboa@MacBook-Pro-de-Luis-7.local>
2016-05-25 23:51:34 -0500

    Fix bugs

--- Pdf.php
+++ Pdf.php
@@ -1,6 +1,6 @@
 <?php
 require_once("dompdf/dompdf_config.inc.php");
-$mysqli = new mysqli('localhost', 'root', '', 'final');
+$mysqli = new mysqli('localhost', 'root', 'kikegp9813', 'final');
 if ($mysqli->connect_errno) {
 	echo "Lo sentimos, este sitio web está experimentando problemas.";
     echo "Error: Fallo al conectarse a MySQL debido a: \n";
